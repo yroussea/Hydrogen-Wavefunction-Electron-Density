be33a2e934068d1d58950c74ada02eb630396a84
yroussea <youenn.rousseau@hotmail.com>
2024-03-27 11:55:29 +0100

    debut des sliders

--- hydrowave.py
+++ hydrowave.py
@@ -1,11 +1,13 @@
 import warnings
 warnings.filterwarnings("ignore")
 import  parsing
+import sys
 
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import matplotlib.patches as patches
+from matplotlib.widgets import Button, Slider
 import scipy
 from scipy import special
 from scipy.constants import physical_constants
@@ -62,7 +64,7 @@ def plot_wave_function(psi, probability_density, ax, color='magma'):
     ax.set_xlabel("x (pm)")
     ax.set_ylabel("z (pm)")
         #add color bar
-    ax = plt.gca()
+    # ax = plt.gca()
     
     ax.imshow(probability_density, cmap=color, extent=(-480, 480, -480, 480))
 
@@ -87,23 +89,117 @@ def update(frame, args, max_frame):
     else:
         n, l, m, a_0_scale_factor = args[0]
 
-    psi = compute_wave_function(n, l, m, a_0_scale_factor, t)
+    psi = compute_wave_function(n, l, int(m), a_0_scale_factor, t)
     probability_density = compute_probability_density(psi)
     plot_wave_function(psi, probability_density, ax)
+
+
+def slider(ax_f, ax, fig):
+    def draw(n, l, m, scale):
+        psi = compute_wave_function(n, l, int(m), scale)
+        probability_density = compute_probability_density(psi)
+        plot_wave_function(psi, probability_density, ax_f)
+
+    init_W = 10
+    axW = fig.add_axes([0.7, 0.2, 0.0225, 0.63]) 
+    axX = fig.add_axes([0.78, 0.2, 0.0225, 0.63]) 
+    axY = fig.add_axes([0.86, 0.2, 0.0225, 0.63]) 
+    axZ = fig.add_axes([0.94, 0.2, 0.0225, 0.63]) 
+    W_slider = Slider(
+        ax=axW,
+        label='n',
+        valmin=0.1,
+        valmax=init_W,
+        valinit=init_W,
+        orientation="vertical"
+    )
+    X_slider = Slider(
+        ax=axX,
+        label='l',
+        valmin=0,
+        valmax=init_W,
+        valinit=0,
+        orientation="vertical"
+    )
+    Y_slider = Slider(
+        ax=axY,
+        label='m',
+        valmin=0,
+        valmax=init_W,
+        valinit=0,
+        orientation="vertical"
+    )
+    Z_slider = Slider(
+        ax=axZ,
+        label='scale',
+        valmin=1,
+        valmax=10,
+        valinit=4,
+        orientation="vertical"
+    )
+    Y_slider.valmax = X_slider.val
+    X_slider.valmax = W_slider.val - 1
+    def Wupdate(val):
+        X_slider.valmax = W_slider.val - 1
+        X_slider.eventson = False
+        X_slider.set_val(min(X_slider.val, W_slider.val - 1))
+        X_slider.eventson = True
+        Y_slider.valmax = X_slider.val
+        Y_slider.eventson = False
+        Y_slider.set_val(min(Y_slider.val, X_slider.val))
+        Y_slider.eventson = True
+        draw(W_slider.val, X_slider.val, Y_slider.val, Z_slider.val)
+        fig.canvas.draw()
+
+    def Xupdate(val):
+        Y_slider.valmax = X_slider.val
+        Y_slider.eventson = False
+        Y_slider.set_val(min(Y_slider.val, X_slider.val))
+        Y_slider.eventson = True
+        draw(W_slider.val, X_slider.val, Y_slider.val, Z_slider.val)
+        fig.canvas.draw()
+
+    def Yupdate(val):
+        draw(W_slider.val, X_slider.val, Y_slider.val, Z_slider.val)
+        fig.canvas.draw()
+
+    def Zupdate(val):
+        draw(W_slider.val, X_slider.val, Y_slider.val, Z_slider.val)
+        fig.canvas.draw()
+
+    W_slider.on_changed(Wupdate)
+    X_slider.on_changed(Xupdate)
+    Y_slider.on_changed(Yupdate)
+    Z_slider.on_changed(Zupdate)
+    draw(W_slider.val, X_slider.val, Y_slider.val, Z_slider.val)
+
+
+
 
 def main():
     global ax
 
     plt.style.use('dark_background')
-    fig, ax = plt.subplots()
+    print(sys.argv)
 
-    max_frame = 100
-    steps = parsing.open_file()
+    if not (len(sys.argv) > 1 and sys.argv[1] == "slider"):
+        fig, ax = plt.subplots(figsize=(12, 10))
+        max_frame = 100
+        steps = parsing.open_file()
+        ani = animation.FuncAnimation(fig, update, frames=max_frame, interval=10, blit=False, fargs=(steps, max_frame,))
 
+    else:
+        fig, (ax, ax2) = plt.subplots(1,2,
+            figsize=(16, 10),
+            gridspec_kw={'width_ratios': [2, 1]})
+        ax2.set_aspect('equal')
+        ax2.axis('off')
 
-    ani = animation.FuncAnimation(fig, update, frames=max_frame, interval=10, blit=False, fargs=(steps, max_frame,))
+        max_frame = 100
+        slider(ax, ax2, fig)
 
     plt.show()
+
 
 if __name__ == "__main__":
     main()
